df37d9378bc0712bbdc93051c4336e0ff25192e3
Eloi du Crest <eloi@letmotiv.io>
2023-08-21 10:41:17 +0200

    set back

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,6 @@ from flask_cors import CORS
 import sqlite3
 
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -31,6 +25,105 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @app.route('/')
+    def index():
+        import RPi.GPIO as GPIO
+        import time
+        from gpiozero import Motor, LED
+        # import custom_motor
+
+        # Module GPIO: BOARD ou BCM (numérotation comme la sérigraphie de la carte ou comme le chip) #
+        GPIO.setmode(GPIO.BCM)
+
+        # Définition des broches GPIO #
+        GPIO_TRIGGER = 16
+        GPIO_ECHO = 12
+
+        # Définition des broches en entrées ou en sortie #
+        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+        motor = Motor(17, 18)
+        motorSwitch = LED(27)
+
+        def toggleSwitch(distance):
+            global motorSpeedForward
+            if distance >= 4:
+                motorSwitch.on()
+                motor.forward(1)
+            elif distance < 4:
+                motorSwitch.off()
+
+        def distance():
+            # Mise à l'état haut de la broche Trigger #
+            GPIO.output(GPIO_TRIGGER, True)
+
+            # Mise à l'état bas de la broche Trigger aprés 10 µS #
+            time.sleep(0.0000001)
+            GPIO.output(GPIO_TRIGGER, False)
+
+            StartTime = time.time()
+            StopTime = time.time()
+
+            # Enregistrement du temps de départ des ultrasons #
+            while GPIO.input(GPIO_ECHO) == 0:
+                StartTime = time.time()
+
+            # Enregistrement du temps d'arrivés des ultrasons #
+            while GPIO.input(GPIO_ECHO) == 1:
+                StopTime = time.time()
+
+            # Calcul de la durée de l'aller-retour des US #
+            TimeElapsed = StopTime - StartTime
+            # On multiplue la durée par la vitesse du son: 34300 cm/s #
+            # et on divise par deux car il s'agit d'un aller et retour. #
+            distance = (TimeElapsed * 34300) / 2
+
+            toggleSwitch(distance)
+
+        while True:
+            distance()
+        return "index"
+
+    @app.route('/test')
+    def test():
+        connection = sqlite3.connect('./database/database.db')
+        connection.row_factory = sqlite3.Row
+        users = connection.execute('SELECT * FROM users').fetchall()
+        data = []
+        for user in users:
+            data.append([x for x in user])
+        connection.close()
+        return data
+
+    @app.route('/servo')
+    def servo():
+        import RPi.GPIO as GPIO
+        import time
+
+        servoPIN = 24
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(servoPIN, GPIO.OUT)
+
+        p = GPIO.PWM(servoPIN, 50)
+        p.start(2.5)
+
+        while True:
+            p.ChangeDutyCycle(4.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(6.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(12.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(8.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(6.5)
+            time.sleep(0.5)
 
     return app
 
